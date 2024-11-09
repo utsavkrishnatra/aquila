f43650fc8a0be2c1e866c9ddb71230692041c338
utsavkk90 <utsavkk90@gmail.com>
2024-11-09 08:18:28 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+// Enqueue theme stylesheet
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'my-theme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
